198986bb5c7a61e5c50784e5b087cfe4c79a62e1
AbhimanyuRana <91766052+AbhumanyuXMusic@users.noreply.github.com>
2021-12-07 08:44:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,14 +11,14 @@ BOT_NAME = getenv("BOT_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 ASSISTANT_USERNAME = getenv("ASSISTANT_USERNAME")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "DeeCodeBots")
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "DeeCodesupport")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "Pubglovers_shayri_lovers")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "AlishaSupport")
 BOT_USERNAME = getenv("BOT_USERNAME")
-BG_IMAGE = getenv("BG_IMAGE", "https://te.legra.ph/file/1b5f32e7b440302ac6435.png")
-THUMB_IMG = getenv("THUMB_IMG", "https://te.legra.ph/file/1b5f32e7b440302ac6435.png")
-BOT_IMG = getenv("BOT_IMG", "https://te.legra.ph/file/1b5f32e7b440302ac6435.png")
-AUD_IMG = getenv("AUD_IMG", "https://te.legra.ph/file/1b5f32e7b440302ac6435.png")
-QUE_IMG = getenv("QUE_IMG", "https://te.legra.ph/file/1b5f32e7b440302ac6435.png")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/a7adee6cf365d74734c5d.png")
+THUMB_IMG = getenv("THUMB_IMG", "https://telegra.ph/file/a7adee6cf365d74734c5d.png")
+BOT_IMG = getenv("BOT_IMG", "https://telegra.ph/file/de138de8fd880becb9cf1.jpg")
+AUD_IMG = getenv("AUD_IMG", "https://telegra.ph/file/de138de8fd880becb9cf1.jpg")
+QUE_IMG = getenv("QUE_IMG", "https://telegra.ph/file/a7adee6cf365d74734c5d.png")
 
 admins = {}
 API_ID = int(getenv("API_ID"))
@@ -26,7 +26,7 @@ API_HASH = getenv("API_HASH")
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "70"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "300"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
